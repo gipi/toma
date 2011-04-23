204136548b08c219e8a3076f3d776a0ade3b9560
Gianluca Pacchiella <gianluca.pacchiella@ktln2.org>
2011-04-23 17:17:09 +0200

    Initial import.
    Project robben at
    <http://invisibleroads.com/tutorials/geodjango-googlemaps-build.html>.

---
+++ waypoints/models.py
@@ -0,0 +1,18 @@
+# Import django modules
+from django.contrib.gis.db import models
+from django.contrib.gis import admin
+
+
+class Waypoint(models.Model):
+
+    name = models.CharField(max_length=32)
+    geometry = models.PointField(srid=4326)
+    objects = models.GeoManager()
+
+    def __unicode__(self):
+        return '%s %s %s' % (self.name, self.geometry.x, self.geometry.y)
+
+class WaypointAdmin(admin.OSMGeoAdmin):
+	pass
+
+admin.site.register(Waypoint, WaypointAdmin)
